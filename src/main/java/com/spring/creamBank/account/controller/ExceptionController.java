f4f8b34f4f336a60bc1c33007a70692918bde177
gw8413 <gw8413@gmail.com>
2024-01-05 22:22:11 +0900

    feat: Account 송금 기능 추가

---
+++ src/main/java/com/spring/creamBank/account/controller/ExceptionController.java
@@ -0,0 +1,2 @@
+package com.spring.creamBank.account.controller;public class ExceptionController {
+}
--- src/main/java/com/spring/creamBank/account/dto/AccountWithdrawal.java
+++ src/main/java/com/spring/creamBank/account/dto/AccountWithdrawal.java
@@ -3,24 +3,26 @@ package com.spring.creamBank.account.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @ToString
 @Getter
-public class AccountDeposit {
+@Setter
+public class AccountWithdraw {
 
     @NotBlank(message = "계좌번호를 입력해주세요.")
     private  String accountNumber;
     @NotBlank(message = "예금주명을 입력해주세요.")
     private  String owner;
 
-    @NotBlank(message = "예금할 금액을 입력해주세요.")
-    private  Long deposit;
+    @NotBlank(message = "출금할 금액을 입력해주세요.")
+    private  Long withdrawalAmount;
 
     @Builder
-    public AccountDeposit(String accountNumber, String owner, Long deposit) {
+    public AccountWithdraw(String accountNumber, String owner, Long withdrawalAmount) {
         this.accountNumber = accountNumber;
         this.owner = owner;
-        this.deposit = deposit;
+        this.withdrawalAmount = withdrawalAmount;
     }
 }
